bf06a5812b278d37a3db202231da583f90618660
Gergely Imreh <imrehg@gmail.com>
2016-08-02 22:53:59 +0800

    clean up code

--- artik/common.py
+++ artik/common.py
@@ -1,29 +1,28 @@
 """
-Common functions for the ARTIK Cloud device, implementing
+Common functions for the ARTIK Cloud device, implementing sensor readings,
+and device actions as defined by the applicable manifest.
 """
-import json
 import os
 import random
-import time
 
 import psutil
 import requests
 
-"""
-Settings
-"""
+###
+# Settings
+###
 DEVICE_ID = os.getenv('ARTIKCLOUD_DEVICE_ID')
 DEVICE_TOKEN = os.getenv('ARTIKCLOUD_DEVICE_TOKEN')
 try:
     READINGS_PERIOD = int(os.getenv('READINGS_PERIOD', 600))
-except Exception as e:
+except ValueError:
     # By default, do a reading every 600s = 10min, not to exhaust
     # the max message quota of the ARTIK Cloud free tier (150msg/day)
     READINGS_PERIOD = 600
 
-"""
-Get readings (output)
-"""
+###
+# Get readings (output)
+###
 def reading_cpu(interval=0):
     """Reading CPU utilization
 
@@ -44,20 +43,21 @@ def reading_random(lower=0, upper=1):
     lower -- lower bound of the interval, default=0
     upper -- upper bound of the interval, default=1
     """
-    if (lower > upper):
+    if lower > upper:
         lower, upper = upper, lower
     return random.uniform(lower, upper)
 
-"""
-Handle actions (input)
-"""
-def setOff():
+###
+# Handle actions (input)
+###
+def action_set_off():
     """Turn device off through the Supervisor API
     """
-    url = "{}/v1/shutdown?apikey={}".format(os.getenv(RESIN_SUPERVISOR_ADDRESS), os.getenv(RESIN_SUPERVISOR_API_KEY))
-    r = requests.post(url)
+    url = "{}/v1/shutdown?apikey={}".format(os.getenv('RESIN_SUPERVISOR_ADDRESS'),
+                                            os.getenv('RESIN_SUPERVISOR_API_KEY'))
+    requests.post(url)
 
-def setText(text=""):
+def action_set_text(text=""):
     """Print text to the log
 
     Arguments:
@@ -66,9 +66,10 @@ def setText(text=""):
     # This could be replaced with any other kind of text display
     print("setText: {}".format(text))
 
-def blinkLed():
+def action_blink_led():
     """Blink the device identification LED (when possible) through the
     Supervisor API
     """
-    url = "{}/v1/blink?apikey={}".format(os.getenv(RESIN_SUPERVISOR_ADDRESS), os.getenv(RESIN_SUPERVISOR_API_KEY))
-    r = requests.post(url)
+    url = "{}/v1/blink?apikey={}".format(os.getenv('RESIN_SUPERVISOR_ADDRESS'),
+                                         os.getenv('RESIN_SUPERVISOR_API_KEY'))
+    requests.post(url)
--- artik/device_mqtt.py
+++ artik/device_mqtt.py
@@ -13,48 +13,55 @@ import paho.mqtt.client as mqtt
 ARTIK_MQTT_URL = "api.artik.cloud"
 ARTIK_MQTT_PORT = 8883
 PUBLISH_CHANNEL = "/v1.1/messages/{}".format(common.DEVICE_ID)
+ACTIONS_CHANNEL = "/v1.1/actions/{}".format(common.DEVICE_ID)
 
-cond = threading.Condition()
-worker_stop = None
+worker_stop = threading.Event()
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
+    """The callback for when the client receives a CONNACK response from the server
+    """
     print("Connected with result code "+str(rc))
 
     # If connected cleanly
     if rc == 0:
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
-        client.subscribe("/v1.1/actions/{}".format(common.DEVICE_ID))
-        global worker_stop
-        worker_stop = threading.Event()
+        client.subscribe(ACTIONS_CHANNEL)
+        # Start readings
+        worker_stop.clear()
         t = threading.Thread(target=worker, args=(client, worker_stop))
         t.daemon = True
         t.start()
 
 def on_disconnect(client, userdata, rc):
+    """Callback for when the client receives disconnects from the server.
+    """
     print("Disconnected with result code "+str(rc))
-    global worker_stop
+    # Stop worker thread
     worker_stop.set()
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+    """Callback for when a PUBLISH message is received from the server
+    """
     print(msg.topic+" "+str(msg.payload))
     try:
         data = json.loads(msg.payload)
-    except Exception as e:
-        raise
-    if "actions" in data:
-        handle_actions(data['actions'])
+    except json.decoder.JSONDecodeError:
+        print("Can't decode payload: {}".format(msg.payload))
+    else:
+        if "actions" in data:
+            handle_actions(data['actions'])
 
 def on_log(client, userdata, level, buf):
+    """Callback on logging event, printing to the device log stream
+    """
     print(buf)
 
 def worker(client, stop):
-    """ Worker thread
+    """ Worker thread: periodic sensor readings and publishing to ARTIK Cloud
     """
     i = 0
-    while (not stop.is_set()):
+    while not stop.is_set():
         if i % common.READINGS_PERIOD == 0:
             readings = {
                 "cpu_load": common.reading_cpu(),
@@ -69,19 +76,20 @@ def worker(client, stop):
         time.sleep(1)
 
 def handle_actions(actions):
+    """Handling incoming actions
+    """
     for action in actions:
         if action['name'] == 'setText':
-            common.setText(action['parameters']['text'])
+            common.action_set_text(action['parameters']['text'])
         elif action['name'] == 'blinkLed':
-            common.blinkLed()
+            common.action_blink_led()
         elif action['name'] == 'setOff':
-            common.setOff()
+            common.action_set_off()
         else:
             print("Unknown action: {}".format(action['name']))
 
 
 if __name__ == "__main__":
-
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
@@ -91,29 +99,5 @@ if __name__ == "__main__":
     print("Connecting!")
     client.username_pw_set(common.DEVICE_ID, password=common.DEVICE_TOKEN)
     client.tls_set(certifi.where())
-    # client.loop_start()
-    # client.connect(ARTIK_MQTT_URL, ARTIK_MQTT_PORT, 60)
     client.connect(ARTIK_MQTT_URL, ARTIK_MQTT_PORT, keepalive=15)
-    # client.loop_start()
     client.loop_forever()
-    # Blocking call that processes network traffic, dispatches callbacks and
-    # handles reconnecting.
-    # Other loop*() functions are available that give a threaded interface and a
-    # manual interface.
-
-    # try:
-    #     while True:
-    #         time.sleep(1)
-    # except KeyboardInterrupt:
-    #     print("Exiting!")
-
-    # while True:
-    #     print(CONNECTED)
-    #     readings = {
-    #         "cpu_load": common.reading_cpu(),
-    #         "free_memory": common.reading_memory(),
-    #         "random": common.reading_random()
-    #         }
-    #     print(readings)
-    #     client.publish(PUBLISH_CHANNEL, json.dumps(readings))
-    #     time.sleep(common.READINGS_PERIOD)
