df8018e7badb402f23d7e72a8427c1302e973d30
Gergely Imreh <imrehg@gmail.com>
2016-08-03 17:11:48 +0800

    update start

--- start.sh
+++ start.sh
@@ -2,8 +2,19 @@
 
 # ARTIKCLOUD_CONNECTION defines the default protocol to talk to ARTIk Cloud,
 # and results in running different code within the app.
-# Possible values: rest, sdk, mqtt, websockets
+# Possible values: mqtt
 # Default value: mqtt
-APP_FILENAME="device_${ARTIKCLOUD_CONNECTION-mqtt}.py"
+case "${ARTIKCLOUD_CONNECTION=mqtt}" in
+  mqtt)
+    echo "Starting MQTT version"
+    ;;
+  *)
+    echo "Uknown ARTIKCLOUD_CONNECTION value: ${ARTIKCLOUD_CONNECTION}"
+    echo "Should be one of: mqtt"
+    while : ; do echo "idling..."; sleep 60; done
+    ;;
+esac
+
+APP_FILENAME="device_${ARTIKCLOUD_CONNECTION}.py"
 
 python "artik/$APP_FILENAME"
